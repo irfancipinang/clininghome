128cb26e57ba7a76c2b2c521e5df46724b0464bd
irfancipinang <muhammadirfanm.2006@gmail.com>
2024-07-18 14:58:06 +0700

    new dashboard

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('arsha');
+    return view('eNno');
+});
+
+Route::get('/admin', function () {
+    return view('admin');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
