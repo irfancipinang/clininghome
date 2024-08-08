1ede06f314689038c6876c4e540bf059d464cae6
irfancipinang <muhammadirfanm.2006@gmail.com>
2024-08-08 12:30:39 +0700

    dashboard baru

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::view('/admin2', 'dashboard2');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
